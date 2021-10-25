b4d064fe414830550e572572d273072305ad7ed4
MarcioShimizu <marcio.shimizu@hotmail.com>
2021-10-25 20:27:18 +0900

    adicionado tqdm e algumas coisas a mais

--- script.py
+++ script.py
@@ -1,18 +1,36 @@
 from pymongo import MongoClient
 from time import sleep
+from tqdm import tqdm
+import warnings
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 client_local = MongoClient(port=27017)
 database_local = client_local.bellamassa
-documents = database_local.pedidos.find()
-collection_destination = database_local.pedidoscs
+collection_documents = database_local.pedidoscs.find()
+collection_destination = database_local.pedidos
+documents = []
 
+print("Iniciando transferencia de banco de dados")
+#coloca todos os documentos dentro da Dict documents
 
-for doc in documents:
+print("Fazendo contagem de Documentos")
+for i in collection_documents:
+  documents.append(i)
+print(f"{len(documents)} Documentos encontrados.")
   
-  try:
-    collection_destination.insert(doc)
-  except:
-    print("erro ao adicionar: ", doc)
-  database_local.pedidos.remove(doc['_id'])
-  print(doc['_id'])
-  
+if len(documents) >= 1:
+  print("Copiando documentos...")
+  for doc in tqdm(documents):
+    sleep(0.1)
+    try:
+      collection_destination.insert_one(doc)
+    except:
+      pass
+    try:
+      database_local.pedidoscs.remove(doc['_id'])
+    except:
+      pass
+else:
+  print("Nenhum documento encontado..")
